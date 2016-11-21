fd2c4c8c0680981c2ee86b93a354c280793bef12
Jeff <jt@aeimarketing.com>
2016-11-21 15:08:21 -0500

    Location

--- js/global.js
+++ js/global.js
@@ -1,11 +1,8 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-    window.open = cordova.InAppBrowser.open;
-}
+
+
 function openLink(link_url){
-
-
-        var ref = window.open(link_url, '_system', 'location=yes');
+    
+        var ref =  cordova.InAppBrowser.open(link_url, '_system', 'location=yes');
         return false;
     
 }
