61b46310847472dbf9396845d91168a423871bdc
Jeff <jt@aeimarketing.com>
2016-11-22 02:01:38 -0500

    Broswer embed

--- js/global.js
+++ js/global.js
@@ -3,8 +3,13 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     window.open = cordova.InAppBrowser.open;
 }
-function openLink(link_url){
-        var ref =  window.open(link_url, '_system', 'location=yes');
+function openLink(link_url,url_bar){
+        var bar='location=no';
+        var target='_self';
+        if(url_bar){
+            bar='location=yes';
+        }
+        var ref =  window.open(link_url, target, bar);
         return false;
     
 }
